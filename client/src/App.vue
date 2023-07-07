e7d2f64af556a7cd38cff54ee15f5b32cd008626
Petr (Desktop) <petrbohac3@seznam.cz>
2023-07-07 21:46:19 +0200

    Initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {Dropdown, ListGroup, ListGroupItem} from "flowbite-vue";
+</script>
+
+<template>
+  <dropdown text="Click me" placement="bottom" style="margin: 250px;">
+    <list-group>
+      <list-group-item>Item #1</list-group-item>
+      <list-group-item>Item #2</list-group-item>
+      <list-group-item>Item #3</list-group-item>
+    </list-group>
+  </dropdown>
+
+
+</template>
+
+<style scoped>
+
+</style>
