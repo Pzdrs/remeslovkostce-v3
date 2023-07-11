7aa6f23b38f1f15fe482c77a1c665f95f0177295
Petr (Desktop) <petrbohac3@seznam.cz>
2023-07-12 00:38:56 +0200

    DOIPCIE KONECNE FUNGUJE API SUBDOMENA

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,4 +3,4 @@
 python -m manage migrate --no-input
 python -m manage collectstatic --no-input
 
-gunicorn backend.wsgi:application --bind 0.0.0.0:8000
+gunicorn backend.wsgi:application --bind 0.0.0.0:8000 --log-level=debug
