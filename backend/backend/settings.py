17677b5618df9db6cad50f9047d23fb166bc3a8c
Petr (Desktop) <petrbohac3@seznam.cz>
2023-08-12 23:18:30 +0200

    environment variables, docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -18,20 +18,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split(' ')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split(',')
 
-CSRF_TRUSTED_ORIGINS = ['http://*.localhost']
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS').split(',')
 CORS_ALLOW_HEADERS = "*"
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:*',
-    'https://remeslovkostce.cz'
-]
+CORS_ORIGIN_WHITELIST = os.environ.get('CORS_ORIGIN_WHITELIST').split(',')
 
 # Application definition
 
